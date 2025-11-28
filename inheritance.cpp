d57b41d8f0c30d7959e6b0385c43dfa66c44bb04
REECARD <reecarddbc@gmail.com>
2025-11-28 13:06:14 +0530

    Implement inheritance with Animal, Dog, and Puppy classes

---
+++ inheritance.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    void eat() { cout << "Eating...\n"; }
+};
+
+class Dog : public Animal {
+public:
+    void bark() { cout << "Barking...\n"; }
+};
+
+class Puppy : public Dog {
+public:
+    void weep() { cout << "Weeping...\n"; }
+};
+
+int main() {
+    Puppy p;
+    p.eat();     // from Animal
+    p.bark();    // from Dog
+    p.weep();    // own function
+}
